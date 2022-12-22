d16e6db8ef304846d643990fd54d1186b73c6825
Maira Joana Hanel <maira.joana.hanel@gmail.com>
2022-12-22 16:39:25 -0300

    get-tasks-cached

--- src/app/features/task/usecases/list-tasks.usecase.ts
+++ src/app/features/task/usecases/list-tasks.usecase.ts
@@ -5,19 +5,16 @@ export class ListTasksUsecase {
     constructor(private repository: TaskRepository, private cacheRepository: CacheRepository) {}
 
     public async execute(userId: string) {
-        const cachedList = await this.cacheRepository.get("tasks");
+        const cachedList = await this.cacheRepository.get(`${userId}tasks`);
         if(cachedList) {
-            return {
-                cache: true,
-                data: cachedList
-            };
+            return cachedList;
         }
 
         const result = await this.repository.list(userId);
 
         const resultJson = result.map(item => item.toJson());
 
-        await this.cacheRepository.set("tasks", resultJson);
+        await this.cacheRepository.set(`${userId}tasks`, resultJson);
 
         return resultJson;
     }
